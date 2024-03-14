aa4735c8cb19f60b65dbb53cc5abc85349c812c2
Richard B. Santos <46171724+Richardbsantos@users.noreply.github.com>
2024-03-14 05:15:58 -0300

    Create Auth pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import './global.css'
 
 import {RouterProvider} from 'react-router-dom'
 import {router} from './routes'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 
 export function App() {
   return (
-    <RouterProvider router={router} /> 
+    <HelmetProvider>
+        <Helmet  titleTemplate="%s | Seraphim.shop"/>
+
+        <RouterProvider router={router}/>
+        
+    </HelmetProvider>
   )
 }
